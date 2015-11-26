a336507448243e2760560e2be45c72817bf3cefc
Louis <trthanhquang@gmail.com>
2015-11-27 01:23:10 +0800

    feedback for hw1 q1

--- hw1_basic_io/home1.cpp
+++ hw1_basic_io/home1.cpp
@@ -1,31 +1,39 @@
+//Comment: Good! Please make sure input/output format match with sameple input output!
+//Question: what is fflush(stdin) for?
+
 #include<stdio.h>
 int main ()
 {
 	int a , b , c, d ;
-	
 	char o;
+    /*
 	printf("input a?\n");
 	scanf("%d",&a);
 	printf("input b?\n");
 	scanf("%d",&b);
 	fflush(stdin);
 	printf("a+b=%d , a-b=%d ,a*b=%d , a/b=%f\n",a+b ,a-b , a*b , double (a)/b);
-    printf("enter operator either + or - or * or / ?\n");
+    */
+    //printf("enter operator either + or - or * or / :");
 	scanf("%c",&o);
-	printf("enter one of result of a+b or a-b or a*b or a/b and input c?");
+    //printf("enter one of result of a+b or a-b or a*b or a/b and input c?");
 	scanf("%d%d",&c,&d);
 	switch (o){
 		case '+' :
-            printf("%d + %d = %d",c, d, c+d);
+            //printf("%d + %d = %d",c, d, c+d);
+            printf("%d",c+d);
             break;
         case '-':
-            printf("%d - %d = %d",c, d, c-d);
+            //printf("%d - %d = %d",c, d, c-d);
+            printf("%d",c-d);
             break;
         case '*':
-            printf("%d * %d = %d",c, d, c*d);
+            //printf("%d * %d = %d",c, d, c*d);
+            printf("%d",c*d);
             break;
         case '/':
-            printf("%d / %d = %d",c, d, c/d);
+            //printf("%d / %d = %d",c, d, c/d);
+            printf("%d",c/d);
             break;
         default:
             printf("Error! operator is not correct");
